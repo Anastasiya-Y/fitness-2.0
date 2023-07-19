5aab4a2f9288046e1b0a8798ddf28e36668746e3
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-07-19 15:07:05 +0300

    Добавляет инициализацию табов

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initVideo} from './modules/init-video';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -22,6 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     initVideo();
+    initTabs();
     form.init();
   });
 });
