d0cec84e3ce5d24933d13b8068b76c8ebf3ade5f
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-07-29 02:39:56 +0300

    Исправляет ошибку в js

--- source/js/modules/init-video.js
+++ source/js/modules/init-video.js
@@ -1,45 +1,58 @@
-const videoGym = document.querySelector('.gym__video-wrapper');
-const videoGymLink = videoGym.querySelector('.gym__video-link');
-const videoGymButton = videoGym.querySelector('.gym__video-button');
+function initVideo() {
+  const video = document.querySelector('.gym__video-wrapper');
 
-function createUrl() {
+  if (!video) {
+    return;
+  }
+
+  createVideo(video);
+}
+
+function createVideo(video) {
+  const link = video.querySelector('.gym__video-link');
+  const button = video.querySelector('.gym__video-button');
+
+  if (!link && !button) {
+    return;
+  }
+
+  const id = createUrl(link);
+
+  video.addEventListener('click', () => {
+    const iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('gym__video-wrapper--is-enabled');
+}
+
+function createUrl(link) {
   const regexp = /https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/i;
-  const linkUrl = videoGymLink.getAttribute('href');
-  const match = linkUrl.match(regexp);
+  const url = link.getAttribute('href');
+  const match = url.match(regexp);
 
   return match[1];
 }
 
-const videoId = createUrl();
-
-function generateURL() {
+function generateUrl(id) {
   let query = '?rel=0&showinfo=0&autoplay=1';
 
-  return 'https://www.youtube.com/embed/' + videoId + query;
+  return 'https://www.youtube.com/embed/' + id + query;
 }
 
-const createIframe = () => {
+function createIframe(id) {
   const iframe = document.createElement('iframe');
 
   iframe.setAttribute('allowfullscreen', '');
   iframe.setAttribute('allow', 'autoplay');
-  iframe.setAttribute('src', generateURL());
+  iframe.setAttribute('src', generateUrl(id));
   iframe.classList.add('gym__video');
 
   return iframe;
-};
-
-const initVideo = () => {
-  videoGym.addEventListener('click', () => {
-    const iframeUser = createIframe();
-
-    videoGymLink.remove();
-    videoGymButton.remove();
-    videoGym.appendChild(iframeUser);
-  });
-
-  videoGymLink.removeAttribute('href');
-  videoGym.classList.add('gym__video-wrapper--is-enabled');
-};
+}
 
 export {initVideo};
